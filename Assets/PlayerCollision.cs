7c6c9c683632117fb2bdba2b6827f665948fed5f
Ftoom1421 <ftoomalgh@gmail.com>
2024-02-21 17:19:04 +0300

    tmatm

--- Assets/PlayerCollision.cs
+++ Assets/PlayerCollision.cs
@@ -3,10 +3,17 @@ using UnityEngine;
 
 public class PlayerCollision : MonoBehaviour
 {
+    public PlayerMovement movement;
+
     // Start is called before the first frame update
-    void OnCollisionEnter()
+    void OnCollisionEnter(Collision collisionInfo)
     {
-        Debug.Log("we hit somthing");
+        if (collisionInfo.collider.name == "Obstacle") ;
+        {
+            movement.enabled = false;
+        }
+
     }
+
 
 }
