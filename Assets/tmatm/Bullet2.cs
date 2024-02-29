d96580ad92431fcaa822ee44e0ce0a4040f997cd
Ftoom1421 <ftoomalgh@gmail.com>
2024-02-29 17:09:52 +0300

    tmatm

--- Assets/tmatm/Bullet2.cs
+++ Assets/tmatm/Bullet2.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Bullet2 : MonoBehaviour
 {
+    private Rigidbody rb;
+
+
     // Start is called before the first frame update
     void Start()
     {
         Destroy(gameObject, 5);
+        rb = GetComponent<Rigidbody>();
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += Vector3.forward;
+        rb.velocity += Vector3.forward;
     }
+    void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+
+        //Destroy(collision.gameObject.tag);
+        if (collision.gameObject.tag == "enemy")
+        {
+            Destroy(collision.gameObject);
+        }
+
+
+    }
+
+
 }
--- Assets/tmatm/player2.cs
+++ Assets/tmatm/player2.cs
@@ -5,37 +5,56 @@ using UnityEngine;
 public class player2 : MonoBehaviour
 {
     [SerializeField] GameObject bullet;
+    [SerializeField] Transform bulletspown;
+    private Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        bool moving = false;
         if (Input.GetKey(KeyCode.UpArrow))
         {
             // this code it,s same transform.position = transform.position + Vector3.forward
-            transform.position += Vector3.forward;
+            rb.velocity += Vector3.forward;
+            moving = true;
         }
         if (Input.GetKey(KeyCode.DownArrow))
         {
-            transform.position += Vector3.back;
+            rb.velocity += Vector3.back;
+            moving = true;
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.position += Vector3.right;
+            rb.velocity += Vector3.right;
+            moving = true;
         }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.position += Vector3.left;
+            rb.velocity += Vector3.left;
+            moving = true;
         }
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Instantiate(bullet, transform.position, Quaternion.identity);
+            Instantiate(bullet, bulletspown.position, Quaternion.identity);
+
         }
+        if (moving == false)
+        {
+            rb.velocity = new Vector3(0, rb.velocity.y, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            rb.AddForce(Vector3.up * 7, ForceMode.Impulse);
+            //rb.AddForce(Vector3.up, ForceMode.Impulse); FOR add force   واذا نبغاه يوقف نحط داون لانهايي يعني يطير ويطيرررررر مايوقف
+
+        }
+
 
     }
 
