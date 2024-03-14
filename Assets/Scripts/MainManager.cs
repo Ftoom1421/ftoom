da8f76c477c1cde648381ffff855257b7a38d967
Ftoom1421 <154553254+Ftoom1421@users.noreply.github.com>
2024-03-14 08:25:44 +0300

    14 march - HOME

---
+++ Assets/Scripts/MainManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainManager : MonoBehaviour
+{
+    public static MainManager Instance;
+    public Color TeamColor; // new varible declared
+
+    public void NewColorSelected(Color color)
+    {
+        MainManager.Instance.TeamColor = color;
+    }
+    private void start()
+    {
+        if (MainManager.Instance != null)
+        {
+            Color(MainManager.Instance.TeamColor);
+        }
+    }
+    private void Awake()
+   {
+    // start of new code
+    if( Instance != null)
+    {
+        Destroy (gameObject);
+        return;
+    }
+    Instance = this;
+    DontDestroyOnLoad(gameObject);
+   } 
+}
